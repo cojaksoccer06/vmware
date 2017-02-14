4b5099445434617edd5ac34928d799cffa404068
cojaksoccer06 <cojaksoccer06@users.noreply.github.com>
2017-02-14 10:21:05 -0600

    Removing File
    Removing and moving to a new location

--- scripts/vSphereLogon.ps1
+++
@@ -1,6 +0,0 @@
-$vCenterInstance = "VCENTER FQDN HERE"
-$vCenterUser = "VCENTER USER ACCOUNT HERE"
-$vCenterPass = "VCENTER PASSWORD HERE"
-
-#Connects to the vCenter with Username and password
-Connect-VIServer $vCenterInstance -User $vCenterUser -Password $vCenterPass -WarningAction SilentlyContinue
